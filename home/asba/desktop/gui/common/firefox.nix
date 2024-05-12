4e1ae3c1f75faeff21ed30b2f4a2de149d4de883
Åsmund Balke <77496597+balkenix@users.noreply.github.com>
2024-05-12 20:31:01 +0100

    bookmarks

--- home/asba/desktop/gui/common/firefox.nix
+++ home/asba/desktop/gui/common/firefox.nix
@@ -9,6 +9,28 @@
       settings = {
         "extensions.autoDisableScopes" = 0;
       };
+      bookmarks = [
+        {
+          name = "NVF Manual";
+          url = "https://notashelf.github.io/nvf/options.html";
+          keyword = "nvf";
+        }
+        {
+          name = "HM Options";
+          url = "https://home-manager-options.extranix.com";
+          keyword = "hm";
+        }
+        {
+          name = "NixOS Search";
+          tags = ["nixos" "nix"];
+          keyword = "nixos";
+          url = "https://search.nixos.org";
+        }
+        {
+          name = "GitHub";
+          url = "https://github.com";
+        }
+      ];
     };
   };
 }
