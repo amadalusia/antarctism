6f0e5fa19e7816da5a54cf9e9aafa1172459c9e9
Åsmund Balke <77496597+balkenix@users.noreply.github.com>
2024-05-16 19:43:06 +0100

    nvf: fix

--- home/asba/desktop/editors/nvf/default.nix
+++ home/asba/desktop/editors/nvf/default.nix
@@ -5,70 +5,77 @@
 
   programs.nvf = {
     enable = true;
-    vim = {
-      package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
-      autoIndent = true;
+    settings = {
+      vim = {
+        package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
+        autoIndent = true;
 
-      lsp = {
-        enable = true;
-        formatOnSave = true;
-        lightbulb.enable = true;
-        lspSignature.enable = true;
-        lsplines.enable = true;
-        lspconfig.enable = true;
-        lspkind.enable = true;
-        lspsaga.enable = true;
-        null-ls.enable = true;
-        nvimCodeActionMenu.enable = true;
-      };
+        lsp = {
+          enable = true;
+          formatOnSave = true;
+          lightbulb.enable = true;
+          lspSignature.enable = true;
+          lsplines.enable = true;
+          lspconfig.enable = true;
+          lspkind.enable = true;
+          lspsaga.enable = true;
+          null-ls.enable = true;
+          nvimCodeActionMenu.enable = true;
+        };
 
-      git = {
-        enable = true;
-        gitisigns.enable = true;
-      };
+        visuals = {
+          nvimWebDevicons.enable = true;
+        };
 
-      ui = {
-        noice.enable = true;
-      };
+        git = {
+          enable = true;
+          gitsigns.enable = true;
+        };
 
-      snippets.vsnip.enable = true;
-      spellcheck.enable = true;
+        ui = {
+          noice.enable = true;
+        };
 
-      startPlugins = [
-        "plenary-nvim"
-        "nui-nvim"
-      ];
-      
-      autocomplete = {
-        enable = true;
-        type = "nvim-cmp";
-      };
+        snippets.vsnip.enable = true;
+        spellcheck.enable = true;
 
-      statusline.lualine = {
-        enable = true;
-        icons.enable = true;
-        theme = "auto";
-      };
+        startPlugins = [
+          "plenary-nvim"
+          "nui-nvim"
+        ];
+        
+        autocomplete = {
+          enable = true;
+          type = "nvim-cmp";
+        };
 
-      theme = {
-        name = "rose-pine";
-        style = "main";
-      };
+        statusline.lualine = {
+          enable = true;
+          icons.enable = true;
+          theme = "auto";
+        };
 
-      tabline.nvimBufferline = {
-        enable = true;
-      };
+        theme = {
+          enable = true;
+          name = "rose-pine";
+          style = "main";
+        };
 
-      languages = {
-        nix.enable = true;
-        ts.enable = true;
-        rust.enable = true;
-        python.enable = true;
-        lua.enable = true;
-        java.enable = true;
-        go.enable = true;
-        clang.enable = true;
-        zig.enable = true;
+        tabline.nvimBufferline = {
+          enable = true;
+        };
+
+        languages = {
+          nix.enable = true;
+          ts.enable = true;
+          rust.enable = true;
+          python.enable = true;
+          lua.enable = true;
+          java.enable = true;
+          go.enable = true;
+          clang.enable = true;
+          zig.enable = true;
+        };
       };
     };
   };
